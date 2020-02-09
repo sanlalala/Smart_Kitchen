30d0e942d153f439bcdb4e6b35e6da7222a7669e
Matthias Fruth <matthias.fruth0@gmail.com>
2020-02-09 22:45:59 +0100

    Adjust Read.py

--- Read.py
+++ Read.py
@@ -3,23 +3,26 @@
 import socket
 import pickle
 import RPi.GPIO as GPIO
+import time
 from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 HOST = '192.168.0.30'  # The server's hostname or IP address
 PORT = 65432            # The port used by the server
 
-try: 
-    id, text = reader.read()
-   
-finally:
-    GPIO.cleanup()
+while True:
+    try:
+        id, text = reader.read()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    event = [str(id), "U_1_0"]
-    data = pickle.dumps(event)
-    s.sendall(data)
-    data = s.recv(1024)
+    finally:
+        GPIO.cleanup()
 
-print('Received', repr(data))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        event = [str(id), "U_1_0"]
+        data = pickle.dumps(event)
+        s.sendall(data)
+        data = s.recv(1024)
+
+    print('Received', repr(data))
+    time.sleep(1)

c8d8825d2acad27a9da60e48cf9221281711be12
Matthias <matthias.fruth@stud.uni-regensburg.de>
2020-02-10 10:13:16 +0100

    Fix stuff

--- Python_Server/client.py
+++ Python_Server/client.py
@@ -2,16 +2,21 @@
 
 import socket
 import pickle
+import time
 
 HOST = '192.168.178.52'  # The server's hostname or IP address
 PORT = 65432            # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    event = ["384192513797", "U_1_0"]
-    data = pickle.dumps(event)
-    s.sendall(data)
-    # data = s.recv(1024)
-    s.close()
+while True:
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        event = ["384192513797", "U_1_0"]
+        data = pickle.dumps(event)
+        s.sendall(data)
+        # data = s.recv(1024)
+        s.close()
+
+    time.sleep(1)
 
 #print('Received', repr(data))
--- Read.py
+++ Read.py
@@ -7,7 +7,7 @@ import time
 from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
-HOST = '192.168.0.30'  # The server's hostname or IP address
+HOST = '169.254.169.181'  # The server's hostname or IP address
 PORT = 65432            # The port used by the server
 
 while True:
@@ -20,9 +20,9 @@ while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         event = [str(id), "U_1_0"]
+        print("Es passiert was")
         data = pickle.dumps(event)
         s.sendall(data)
-        # data = s.recv(1024)
 
-    # print('Received', repr(data))
-    time.sleep(1)
+    GPIO.cleanup()
+    time.sleep(3)
